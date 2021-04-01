508220f56080c99e3eaf86f7e511f65d061ce617
garvit2126 <garvit2126@users.noreply.github.com>
2021-04-02 02:59:07 +0530

    Lets Join

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react';
 import './App.css';
 import Layout from "./hoc/Layout/Layout"
+import Login from './component/Login/Login'
+import Password from './component/Password/Password'
+import ResetPassword from './component/ResetPassword/ResetPassword'
 
 class App extends Component {
-    state = {}
+    state = {
+        isAuthenticated : false
+    }
+
+    
+    checkValidity = () => {
+        if(Cookies.get('token')){
+            this.setState({isAuthenticated: true})
+        }
+    }
+
+    componentDidMount(){
+        if(Cookies.get('token')){
+            this.setState({isAuthenticated: true})
+        }
+    }
+
 
    render(){
+        let route = (
+            <Switch>
+                <Route exact path="/reset-password/:id" component={ResetPassword} />
+                <Route exact path="/forgot-password" component={Password}/>
+                <Route exact path="/FAQ" component={FAQ}/>
+                <Route exact path="/" render={()=><Login refresh={this.checkValidity} />} />
+                <Route component={FourOFour} />
+            </Switch>
+        )
+
+        if(this.state.isAuthenticated){
+            route = <Layout />
+        }
+
+
        return (
-            <Layout/>
+            <div>
+                {route}
+                {this.state.isAuthenticated ? null : <Route component={Footer} /> } 
+            </div>
            )
    }
 }
